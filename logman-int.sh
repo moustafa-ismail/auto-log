d83537b13eae9a5cdeda8c378fe1a2a754555a75
moustafa <moustafa.i.reda@gmail.com>
2024-03-13 19:07:16 -0400

    Added file reading name age country and checking for valid age

---
+++ logman-int.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+#
+
+# This files read user names age and country
+#
+
+echo 'Enter your name'
+read name
+echo 'Enter your age'
+
+read age
+
+if [[ $age =~ [0-9] ]] && [[ $age>0 ]] && [[ $age<160 ]]; then 
+	echo 'Thank u'
+
+else
+	echo 'sorry enter a valid age'
+	age=''
+
+fi
+
+
+echo 'Enter your country'
+read country
+
+echo ''
+echo ''
+
+echo "Hello $name"
+echo "Your age is $age"
+echo "You are from $country"
+
+exit 0
